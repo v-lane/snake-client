7df1da4b2b53a8e749a0604cc98620537e1c2f25
v-lane <victoria.lane.m@gmail.com>
2024-02-14 16:51:28 -0500

    created seperate input file for input function

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {
+  setupInput,
+  handleUserInput
+};
--- play.js
+++ play.js
@@ -1,22 +1,10 @@
 
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 // establishes a connection with the game server
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
 
 console.log("Connecting ...");
 connect();
